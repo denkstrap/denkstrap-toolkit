a1444607571a46b1432441d3d8fe79277ba918c6
Marius Büscher <marius.buescher@denkwerk.com>
2017-06-06 19:03:46 +0200

    Add some documentation for the cache service

--- src/cache/cache.service.js
+++ src/cache/cache.service.js
@@ -1,20 +1,49 @@
+/**
+ * # The cache service
+ *
+ * This is a simple service for providing a cache system in the frontend. This
+ * one is an object cache saving the values in an object. It will not be persitent
+ * between requests.
+ */
 export class Cache {
+
+    /**
+     * @constructor
+     */
     constructor() {
         this.cacheMap = {};
     }
 
+    /**
+     * Get a value from the cache
+     *
+     * @param {String} key
+     * @return {mixed}
+     */
     getValue( key ) {
         this.checkKey( key );
 
         return this.cacheMap[ key ] || false;
     }
 
+    /**
+     * Set a value
+     *
+     * @param {String} key
+     * @param {mixed} value
+     */
     setValue( key, value ) {
         this.checkKey( key );
 
         this.cacheMap[ key ] = value;
     }
 
+    /**
+     * Check for the type of the key
+     *
+     * @param {String} key
+     * @throws {TypeError}
+     */
     checkKey( key ) {
         if ( typeof key !== 'string' ) {
             throw new TypeError( 'Key must be a string' );
